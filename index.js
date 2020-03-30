e448774c0326b67163aac9a0bc0ad3c4116bdb3b
AleeyCreative <babaali196@gmail.com>
2020-03-30 23:20:21 +0100

    added css files and setup basic css imports

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {render} from 'react-dom'
+import "./styling/index.css"
+
+ function App() {
+   return(
+     <div className='mdi mdi-book'>
+     </div>
+   )
+ }
+
+ render(<App/>, document.querySelector('#root'))

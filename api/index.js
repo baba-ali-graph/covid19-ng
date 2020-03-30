5aa0b857dcb40911928175e39251f17ad84200b5
AleeyCreative <babaali196@gmail.com>
2020-03-30 22:36:27 +0100

    created components and api endpoints

---
+++ api/index.js
@@ -0,0 +1,30 @@
+const endpoint = "https://covid19.mathdro.id/api/countries/Nigeria"
+const compareEndpoint = (country) => `https://covid19.mathdro.id/api/countries/${country}`
+
+export async function get(){
+  try {
+  let response = await fetch(endpoint)
+  response = await response.json()
+  console.log(response)
+  return response
+  }
+  catch(e){
+    console.error(e)
+    return ERROR
+  }
+}
+
+export async function compare(country){
+  try {
+  let response = fetch(compareEndpoint(country))
+  response = await response.json()
+  console.log(response)
+  return response
+  }
+  catch(e){
+    console.error(e)
+    return false
+  }
+}
+
+export const ERROR = "Oops ! An error occured, Can you try again ?"
---
+++ components/index.js
@@ -0,0 +1,15 @@
+import AppHeader from './AppHeader'
+import Covid19Table from './Covid19Table'
+import DayBanner from './DayBanner'
+import NewsUpdate from './NewsUpdate'
+import SocialButtons from './SocialButtons'
+import WelcomeText from './WelcomeText'
+
+export default {
+  AppHeader,
+  DayBanner,
+  WelcomeText,
+  Covid19Table,
+  NewsUpdate,
+  SocialButtons 
+}

fe41674e7f1c61a9f1edeece15d3776c063d94ac
AleeyCreative <babaali196@gmail.com>
2020-04-03 09:43:29 +0100

    in progress : api integration

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {render} from 'react-dom'
+import {fetchNigeriaData} from './api/index'
 import AppHeader from './components/AppHeader'
 import Covid19Table from './components/Covid19Table'
 import DayBanner from './components/DayBanner'
@@ -12,13 +13,24 @@ import SideInfo from './components/SideInfo'
 import References from './components/References'
 import store from './store/index'
 import {Provider} from 'react-redux'
-import {getBaseCountry} from './tools/index'
+// import {getBaseCountry} from './tools/index'
 
 import "./styling/index.css"
 import "./styling/base.css"
 
  function App() {
-    const baseCountry = getBaseCountry()
+    const [baseCountry, setBaseCountry] = useState(null)
+    useEffect(() => {
+    let aborter = new AbortController()
+    async function initFetchData(){
+        let baseCountry_ = await fetchNigeriaData()
+        setBaseCountry({...baseCountry})
+    }
+    initFetchData()
+    return () => {
+        aborter.abort()
+    }
+})
    return(
      <div className='app-body'>
      <Provider store={{...store, baseCountry:baseCountry ? baseCountry : null}}>

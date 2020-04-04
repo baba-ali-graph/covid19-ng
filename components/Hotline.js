d883c0d0db0b0b1ad8b44b78e7aa550faac7549e
AleeyCreative <babaali196@gmail.com>
2020-04-04 08:01:59 +0100

    done : NCDC Hotlines added

--- components/Hotline.js
+++ components/Hotline.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Cards from './components/Cards'
 
 export default function(){
-const hotlines = ['0930495-CORONA']
+const hotlines = ['Whatsapp number : +234 708 711 0839', 'SMS number : +234 809 955 5577', ' Toll-free number : 0800 9700 0010']
     return(
         <div className='card-hotline'>
             <h2 className='card-hotline-title'> NCDC Hotlines </h2>

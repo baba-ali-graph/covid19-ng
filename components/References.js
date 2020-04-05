5850f5778f88e72633682e5c167fc06734e2bb55
AleeyCreative <babaali196@gmail.com>
2020-04-05 12:19:58 +0100

    in progress: UI changes

--- components/References.js
+++ components/References.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React from "react";
 
-export default function References({references}){
+export default function References({ references }) {
   return (
-    <div className='references-container'>
-    <h2> Sources </h2>
-    {references.map(reference => <a  href={reference} className='reference' key={reference}> {reference} </a>)}
-    </div>
-  )
-}
+    <div className="references-container">
+      <h2> Sources </h2>
+      <ol>
+        <li>
+          This
+          <a href="https://guardian.ng/news/nigeria-confirms-first-case-of-coronavirus/">
+            {" "}
+            article{" "}
+          </a>{" "}
+          gives the date of the first confirmed case in nigeria
+        </li>
 
-References.defaultProps = {
-  references : ["https://guardian.ng/news/nigeria-confirms-first-case-of-coronavirus/",
-  ]
+        <li> Real-time update is provided by this <a href='https://covid-19.mathdro.id/api'> API </a>.
+        </li>
+      </ol>
+    </div>
+  );
 }

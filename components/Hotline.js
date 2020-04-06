904bebf12828105294ada63a5fce2b5be07021ad
AleeyCreative <babaali196@gmail.com>
2020-04-06 11:21:48 +0100

    in progress : table styling

--- components/Hotline.js
+++ components/Hotline.js
@@ -2,13 +2,13 @@ import React from 'react'
 import Cards from './components/Cards'
 
 export default function(){
-const hotlines = ['Whatsapp number : +234 708 711 0839', 'SMS number : +234 809 955 5577', ' Toll-free number : 0800 9700 0010']
+const hotlines = ['Whatsapp No : +234 708 711 0839', 'SMS No : +234 809 955 5577', ' Toll-free No : 0800 9700 0010']
     return(
         <div className='card-hotline'>
             <h2 className='card-hotline-title'> NCDC Hotlines </h2>
             
             <p>
-                {hotlines.map(ht => <span key={ht}> {ht} </span>)}
+                {hotlines.map(ht => <div className='ht' key={ht}> {ht} </div>)}
             </p>
         </div>
     )
